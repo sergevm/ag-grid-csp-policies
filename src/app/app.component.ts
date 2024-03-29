306ca80da3090613028f6e95ad57f75a1e1bede4
Serge Van Meerbeeck <serge.vanmeerbeeck@gmail.com>
2024-03-29 12:01:19 +0100

    Upgrade to 31.2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wkb-csp';
+
+ // Each Column Definition results in one Column.
+ public columnDefs: ColDef[] = [
+   { field: 'make'},
+   { field: 'model'},
+   { field: 'price', valueGetter: () => 3 * 2 }
+   // SWITCH COMMENTING HERE TO OBSERVE THE CSP POLICY ISSUES
+  //  { field: 'price', valueGetter: "3 * 2" }
+ ];
+
+ // DefaultColDef sets props common to all Columns
+ public defaultColDef: ColDef = {
+   sortable: true,
+   filter: true,
+ };
+
+ // Data that gets displayed in the grid
+ public rowData$!: Observable<any[]>;
+
+ // For accessing the Grid's API
+ @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+
+ constructor(private http: HttpClient) {}
+
+ // Example load data from sever
+ onGridReady(params: GridReadyEvent) {
+   this.rowData$ = this.http
+     .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+ }
+
+ // Example of consuming Grid Event
+ onCellClicked( e: CellClickedEvent): void {
+   console.log('cellClicked', e);
+ }
+
+ // Example using Grid's API
+ clearSelection(): void {
+   this.agGrid.api.deselectAll();
+ }
 }
